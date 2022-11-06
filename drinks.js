5a0299c85255106203a0f67605ebdeb8514b8424
Lucas Bolla <lucasbolla@icloud.com>
2022-11-06 14:35:27 +0000

    Drinks Exercise

---
+++ drinks.js
@@ -0,0 +1,11 @@
+const nameUser = "Lucas";
+const age = 2022 - 2005;
+const minage = 21;
+
+console.log(age);
+
+if (age >= minage) {
+    console.log(`${nameUser} you can drink alchool !`)
+} else {
+    console.log(`${nameUser} you can't drink alchool !`)
+}
