691db72b5631ee4e1e94de63a27ba85dc141c5c2
Lucas Bolla <lucasbolla@icloud.com>
2022-11-07 14:47:08 +0000

    New Files

---
+++ array.js
@@ -0,0 +1,15 @@
+const notas = [6, 7, 10, 8.5];
+
+console.log(notas);
+
+notas.pop();
+
+console.log(notas);
+
+notas.push(8);
+
+console.log(notas);
+
+let text = `All pontuation is here ${notas}`;
+
+console.log(text);
---
+++ media.js
@@ -0,0 +1,8 @@
+const note1 = 10;
+const note2 = 6;
+const note3 = 7.5;
+const note4 = 3;
+
+const media = (note1 + note2 + note3 + note4) / 4;
+
+console.log(media.toFixed(2));
